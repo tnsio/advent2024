249a4118056303c5f056b6b2ced2a93b4d67db9b
tnsio <ioan.tanasa99@gmail.com>
2024-12-30 17:08:26 +0200

    complete day 3 part 2

--- src/bin/3_part_2.rs
+++ src/bin/3_part_2.rs
@@ -1,3 +1,31 @@
+use std::{fs::File, io::Read};
+
+use regex::Regex;
+
 fn main() {
-    println!("Hello, world!");
+    let mut input_file = File::open("./inputs/3.txt").expect("Could not open input file.");
+    let mut input = String::new();
+
+    input_file
+        .read_to_string(&mut input)
+        .expect("Could not read input file to string.");
+
+    let regex = Regex::new(r"(?<mul>mul\((?<left>\d+),(?<right>\d+)\))|(?<do>do\(\))|(?<dont>don't\(\))").unwrap();
+
+    let mut result : i64 = 0;
+    let mut mult_active = true;
+
+    for cap in regex.captures_iter(&input) {
+        if let Some(_) = cap.name("mul") {
+            if mult_active {
+                result += &cap["left"].parse::<i64>().unwrap() * cap["right"].parse::<i64>().unwrap();
+            }
+        } else if let Some(_) = cap.name("do") {
+            mult_active = true;
+        } else if let Some(_) = cap.name("dont") {
+            mult_active = false;
+        }
+    }
+
+    println!("Result = {}", result);
 }
