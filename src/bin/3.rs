f0f6f6c1efae53c6b878d80d9c2f9f1187524e0b
tnsio <ioan.tanasa99@gmail.com>
2024-12-30 16:12:34 +0200

    complete day 03 part 1

--- src/bin/3.rs
+++ src/bin/3.rs
@@ -1,3 +1,22 @@
+use std::{fs::File, io::Read};
+
+use regex::Regex;
+
 fn main() {
-    println!("Hello, world!");
+    let mut input_file = File::open("./inputs/3.txt").expect("Could not open input file.");
+    let mut input = String::new();
+
+    input_file
+        .read_to_string(&mut input)
+        .expect("Could not read input file to string.");
+
+    let regex = Regex::new(r"mul\((\d+),(\d+)\)").unwrap();
+
+    let mut result : i64 = 0;
+
+    for (_, [left, right]) in regex.captures_iter(&input).map(|cap| cap.extract()) {
+        result += left.parse::<i64>().unwrap() * right.parse::<i64>().unwrap();
+    }
+
+    println!("Result = {}", result);
 }
